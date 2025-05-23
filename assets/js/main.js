e5254b94781a76642fceb4e2ed395f3605d7a14d
Mudassir Nawaz Javaid <mudassirnawazjavaid@gmail.com>
2025-05-24 01:28:22 +0500

    changes made

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,7 +7,7 @@ document.addEventListener('DOMContentLoaded', () => {
     AOS.init();
 
     // Swiper initialization
-    const swiper = new Swiper('.hero-swiper', {
+    const heroSwiper = new Swiper('.hero-swiper', {
         loop: true,
         autoplay: true,
         effect: 'flip',
@@ -22,17 +22,42 @@ document.addEventListener('DOMContentLoaded', () => {
         },
     });
 
+    const goalSwiper = new Swiper('.goals-swiper', {
+        loop: true,
+        navigation: {
+            nextEl: '.goals-next-slide',
+            prevEl: '.goals-prev-slide',
+        },
+        on: {
+            init: function() {
+                const pagination = document.querySelector('.goals-pagination');
+                if (pagination) {
+                    pagination.textContent = this.realIndex + 1;
+                }
+            },
+            slideChange: function() {
+                const pagination = document.querySelector('.goals-pagination');
+                if (pagination) {
+                    pagination.textContent = this.realIndex + 1;
+                }
+            }
+        }
+    });
+
     // Change header on scroll
     window.addEventListener('scroll', () => {
         menu.classList.add('translate-x-full');
         menuToggle.querySelector('.open-menu').classList.remove('hidden');
         menuToggle.querySelector('.close-menu').classList.add('hidden');
-        if (window.scrollY > 0) {
-            header.classList.remove('py-5');
-            header.classList.add('py-3');
-        } else {
-            header.classList.remove('py-3');
-            header.classList.add('py-5');
+        const logo = header.querySelector('.logo');
+        if (logo) {
+            if (window.scrollY > 0) {
+                logo.classList.remove('size-32');
+                logo.classList.add('size-20');
+            } else {
+                logo.classList.remove('size-20');
+                logo.classList.add('size-32');
+            }
         }
     });
 
@@ -41,4 +66,16 @@ document.addEventListener('DOMContentLoaded', () => {
         menuToggle.querySelector('.open-menu').classList.toggle('hidden');
         menuToggle.querySelector('.close-menu').classList.toggle('hidden');
     });
+
+    if (menu.classList.contains('top-34')) {
+        window.addEventListener('scroll', () => {
+            if (window.scrollY > 0) {
+                menu.classList.remove('top-34');
+                menu.classList.add('top-20');
+            } else {
+                menu.classList.remove('top-20');
+                menu.classList.add('top-34');
+            }
+        });
+    }
 });
